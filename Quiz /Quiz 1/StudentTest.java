df5c5470b59bc1f96ddd4eae0375c4694483cd9b
Muhammad Zayeem <muhammadzayeem@hotmail.com>
2025-09-20 15:11:46 +0500

    Add files via upload

---
+++ Quiz /Quiz 1/StudentTest.java
@@ -0,0 +1,27 @@
+public class StudentTest{
+
+public static void main(String[] args){
+
+          Date d=new Date(1,1,2005);
+          Address a=new Address(500,12,"LDA");
+
+          Student s1=new Student("Zayeem",d,a,"Male");
+          Student s2=new Student("Zayeem",d,a,"Male");
+          Student s3=new Student("Zayeem",d,a,"Male");
+          System.out.println(s1);
+          System.out.println(s2);
+          System.out.println(s3);
+          System.out.println("After update ");
+          System.out.println(s1);
+          System.out.println(s2);
+          System.out.println(s3);
+          Student s4=new Student(s1);
+System.out.println("Copied Student:   " + s4);
+
+                if(s1.equals(s2))
+			System.out.println("Student 1 and 2 are same..");
+
+		if(s1.equals(s3))
+			System.out.println("Student 1 and 3 are same..");
+}
+}
